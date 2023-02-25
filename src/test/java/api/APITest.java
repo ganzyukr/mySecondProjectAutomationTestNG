d8c174c0d41d5e57366c65dd379c57b20e20633f
tatiana <ganzyukr@gmail.com>
2023-02-25 19:40:28 +0200

    HW_10 - Tests - the whole lesson in the classroom

--- src/test/java/api/APITest.java
+++ src/test/java/api/APITest.java
@@ -4,12 +4,14 @@ package api;
 import com.sun.javafx.collections.MappingChange;
 import io.restassured.http.ContentType;
 import io.restassured.specification.Argument;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
 
 import java.io.File;
 import java.util.HashMap;
@@ -52,21 +54,77 @@ public class APITest extends BaseApiTest {
                 .body("success", equalTo(true));
     }
 
-        @Test
-        public void checkJsonSchema() {
+    @Test
+    public void checkJsonSchema() {
 
-            given()
-                    .spec(requestSpecification)
-                    .body(reqBody)
-                    .contentType(ContentType.JSON)
-                    .when()
-                    .post()
-                    .then()
-                    .spec(responseSpecification)
-                    .statusCode(200)
-                    .assertThat()
-                    .body(matchesJsonSchema(new File(System.
-                            getProperty("user.dir")+"/src/main/resources/validators/np_address_schema.json")));
+        given()
+                .spec(requestSpecification)
+                .body(reqBody)
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .spec(responseSpecification)
+                .statusCode(200)
+                .assertThat()
+                .body(matchesJsonSchema(new File(System.
+                        getProperty("user.dir") + "/src/main/resources/validators/np_address_schema.json")));
 
-        }
+    }
+
+    @Test
+    public void checkRegions() {
+
+        given()
+                .spec(requestSpecification)
+                .body(reqBody)
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .spec(responseSpecification)
+                .statusCode(200)
+                .assertThat()
+                .body("success", equalTo(true))
+                .body("data[0].Addresses.Present", hasItems("м. Київ, Київська обл.", "смт. Макарів, Макарівський р-н, Київська обл."))
+                .body("data[0].TotalCount", equalTo(106));
+
+    }
+
+    @Test
+    public void checkRegionsOneParamAddresses() {
+
+        given()
+                .spec(requestSpecification)
+                .body(reqBody)
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .spec(responseSpecification)
+                .statusCode(200)
+                .assertThat()
+                .body("success", equalTo(true))
+                .body("data[0].Addresses[0].Present", equalTo("м. Київ, Київська обл."))
+                .body("data[0].TotalCount", equalTo(106));
+    }
+
+    @Test
+    public void checkKyivIsPresentWithPojoUsage() {
+
+        List<Address> addressesData = given()
+                .spec(requestSpecification)
+                .body(reqBody)
+                .contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .spec(responseSpecification)
+                .statusCode(200)
+                .extract()
+                .body().jsonPath().getList("data[0].Addresses", Address.class);
+
+        System.out.println(addressesData);
+        addressesData.forEach(el -> Assertions.assertTrue(el.getPresent().contains("Київ")));
+    }
 }
